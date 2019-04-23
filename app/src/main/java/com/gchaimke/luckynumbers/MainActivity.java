30084aa7fc9c1e10a1c58e47fdc54c0ab46c033b
gchaim <gchaimke@gmail.com>
2019-04-23 16:43:13 +0300

    save data

--- app/src/main/java/com/gchaimke/luckynumbers/MainActivity.java
+++ app/src/main/java/com/gchaimke/luckynumbers/MainActivity.java
@@ -1,18 +1,27 @@
 package com.gchaimke.luckynumbers;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 
 import org.jsoup.Jsoup;
@@ -27,6 +36,8 @@ public class MainActivity extends AppCompatActivity {
     private TextView parsedHtmlNode;
     private ArrayList<String> mArrNumbers = new ArrayList<>();
     StringBuilder sb = new StringBuilder();
+    String settingsFile="app_settings";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,10 +52,12 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                //parsedHtmlNode.clearComposingText();
                 JsoupAsyncTask jsoupAsyncTask = new JsoupAsyncTask();
                 jsoupAsyncTask.execute();
             }
         });
+
     }
 
     @Override
@@ -92,7 +105,8 @@ public class MainActivity extends AppCompatActivity {
                     sb.append(x).append(" set:").append(mArrNumbers.get(x)).append("\n");
                     //htmlContentInStringFormat += x+" set: "+mArrNumbers.get(x)+"\n";
                 }
-            } catch (IOException e) {
+                writeToFile("test",getBaseContext());
+             } catch (IOException e) {
                 sb.append("Error getting data :\n"+e.toString());
                 e.printStackTrace();
             }
@@ -101,7 +115,50 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(Void result) {
-            parsedHtmlNode.setText(sb.toString());
+            int rNum=randomWithRange(11,15);
+            String numsFromSet=getSettings(getBaseContext());
+            //numsFromSet = settings.getString("numbers","No data");
+            parsedHtmlNode.setText(numsFromSet+"\n"+rNum);
         }
+    }
+
+    private void writeToFile(String data,Context context) {
+        try {
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(context.openFileOutput(settingsFile, Context.MODE_PRIVATE));
+            outputStreamWriter.write(data);
+            outputStreamWriter.close();
+        }
+        catch (IOException e) {
+            Log.e("Exception", "File write failed: " + e.toString());
+        }
+    }
+
+    private String getSettings(Context context){
+        String ret="from settings";
+        try{
+            InputStream inputStream = context.openFileInput(settingsFile);
+            if(inputStream!=null){
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader bufferedReader =new BufferedReader(inputStreamReader);
+                String receiveString = "";
+                StringBuilder stringBuilder = new StringBuilder();
+
+                while((receiveString=bufferedReader.readLine())!=null){
+                    stringBuilder.append(receiveString).append("\n");
+                }
+                inputStream.close();
+                ret=stringBuilder.toString();
+            }
+        }catch(FileNotFoundException e){
+
+        }catch (IOException e){
+            Log.e("log Activity","Can not resd file: "+ e.toString());
+        }
+        return ret;
+    }
+
+    int randomWithRange(int min,int max){
+        int range=max-min+1;
+        return (int)(Math.random()*range)+min;
     }
 }
